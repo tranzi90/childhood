527e552b46dcf795e8cd8bfbd176ffd7d0175c09
denis_levchenko <tranzi90@gmail.com>
2022-09-22 00:11:41 +0300

    add functions.php, apply styles

--- wp-content/themes/childhood/functions.php
+++ wp-content/themes/childhood/functions.php
@@ -1,2 +1,6 @@
 <?php
-add_action('wp_enqueue_scripts', );
+add_action('wp_enqueue_scripts', 'childhood_scripts');
+
+function childhood_scripts() {
+	wp_enqueue_style('childhood-style', get_stylesheet_uri());
+}
